cbf41292b052e154714d6deb5a36258ce8f5c07d
Alina Khomenko <alina210198@yandex.ru>
2023-06-04 21:00:30 +0300

    Updates

---
+++ components/PopupWithForm.js
@@ -0,0 +1,19 @@
+
+function PopupWithForm({popup, title,isOpen, form, children, submitButtonText, onClose }) {
+    const popupIsOpen = isOpen ? 'popup_opened' : '';
+    return (
+        <div className={`popup ${popup} ${popupIsOpen}`}>
+        
+        <div className="popup__container">
+          <button className="popup__close"  onClick={onClose} type="reset" aria-label="Закрыть"></button>
+          <h2 className="popup__title">{title}</h2>
+          <form className="popup__form popup__form_profile" method="post"   name ={`form-${form}`} noValidate>
+          ${children}
+            <button type="submit" className="popup__input-save"> {submitButtonText}</button>
+          </form>
+        </div>
+      </div>
+    )
+}
+
+export default PopupWithForm;
